ed903e3f6121df29027bc65cd72579fb97ca7abf
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 12:10:26 -0800

    v9

--- source/elements.lua
+++ source/elements.lua
@@ -2,12 +2,12 @@ local Elements = {}
 
 function Elements.CreateButton(TabPage, BData, Theme)
     local Button = Instance.new("TextButton", TabPage)
-    Button.Size = UDim2.new(1, 0, 0, 34)
+    Button.Size = UDim2.new(1, 0, 0, 32)
     Button.BackgroundColor3 = Theme.ElementColor
     Button.Text = BData.Name
     Button.TextColor3 = Theme.TextColor
     Button.Font = Theme.TextFont
-    Button.TextSize = 11
+    Button.TextSize = 16 -- Standard font looks better slightly larger
     Instance.new("UICorner", Button).CornerRadius = UDim.new(0, 10)
     local s = Instance.new("UIStroke", Button)
     s.Color = Theme.BorderColor
@@ -19,22 +19,22 @@ end
 
 function Elements.CreateToggle(TabPage, TData, Theme)
     local Toggle = Instance.new("TextButton", TabPage)
-    Toggle.Size = UDim2.new(1, 0, 0, 34)
+    Toggle.Size = UDim2.new(1, 0, 0, 32)
     Toggle.BackgroundColor3 = Theme.ElementColor
     Toggle.Text = "  " .. TData.Name
     Toggle.TextXAlignment = Enum.TextXAlignment.Left
     Toggle.TextColor3 = Theme.TextColor
     Toggle.Font = Theme.TextFont
-    Toggle.TextSize = 11
+    Toggle.TextSize = 16
     Instance.new("UICorner", Toggle).CornerRadius = UDim.new(0, 10)
     local s = Instance.new("UIStroke", Toggle)
     s.Color = Theme.BorderColor
     s.Thickness = 1.5
     
     local Ind = Instance.new("Frame", Toggle)
-    Ind.Position = UDim2.new(1, -28, 0.5, -9)
-    Ind.Size = UDim2.new(0, 18, 0, 18)
-    Instance.new("UICorner", Ind).CornerRadius = UDim.new(0, 6)
+    Ind.Position = UDim2.new(1, -26, 0.5, -8)
+    Ind.Size = UDim2.new(0, 16, 0, 16)
+    Instance.new("UICorner", Ind).CornerRadius = UDim.new(0, 5)
     
     local state = TData.CurrentValue or false
     local function update() 
