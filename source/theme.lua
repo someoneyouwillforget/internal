d27f82c560aad04f7c7ceb28a86a65f81291b17a
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 01:13:08 -0800

    Update theme.lua

--- source/theme.lua
+++ source/theme.lua
@@ -2,11 +2,11 @@ return {
     Default = {
         TextFont = Enum.Font.GothamBold,
         TextColor = Color3.fromRGB(255, 255, 255),
-        Background = Color3.fromRGB(12, 12, 12),
-        Topbar = Color3.fromRGB(18, 18, 18),
-        Accent = Color3.fromRGB(120, 120, 120),
-        BorderColor = Color3.fromRGB(60, 60, 60),
-        ElementColor = Color3.fromRGB(22, 22, 22),
-        Rounding = UDim.new(0, 10)
+        Background = Color3.fromRGB(10, 10, 10),
+        Topbar = Color3.fromRGB(20, 20, 20),
+        Accent = Color3.fromRGB(255, 255, 255), -- White Accent
+        BorderColor = Color3.fromRGB(255, 255, 255), -- Pure White Border
+        ElementColor = Color3.fromRGB(25, 25, 25),
+        Rounding = UDim.new(0, 15) -- Max Curvature
     }
 }
