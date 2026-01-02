08e0229a1ba83c2e26c59858608ca9bc7986f08f
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 00:16:21 -0800

    Create Theme.lua

---
+++ source/Theme.lua
@@ -0,0 +1,11 @@
+return {
+    Default = {
+        TextFont = Enum.Font.Code,
+        TextColor = Color3.fromRGB(255, 255, 255),
+        Background = Color3.fromRGB(10, 10, 10), -- Deep Black
+        Topbar = Color3.fromRGB(20, 20, 20),     -- Dark Gray
+        Accent = Color3.fromRGB(150, 150, 150),  -- Neutral Gray
+        ElementBackground = Color3.fromRGB(18, 18, 18),
+        ElementStroke = Color3.fromRGB(35, 35, 35)
+    }
+}
