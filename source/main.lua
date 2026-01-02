8e096e7d270c9234f7c5a0232a2b154b431d58d6
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 00:20:38 -0800

    c2

--- source/main.lua
+++ source/main.lua
@@ -1,10 +1,10 @@
 -- INTERNAL LOADER
 local success, Library = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/Library.lua"))()
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/library.lua"))()
 end)
 
 if not success then
-    warn("FATAL ERROR: Could not load Library.")
+    warn("INTERNAL ERROR: Failed to load Library. Check your GitHub links.")
     return nil
 end
 
