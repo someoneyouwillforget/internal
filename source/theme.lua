db69697017e88883eaa3a646bc9bad99cb7a4373
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 12:05:54 -0800

    v8

--- source/theme.lua
+++ source/theme.lua
@@ -1,12 +1,12 @@
 return {
     Default = {
-        TextFont = Enum.Font.GothamBold,
+        TextFont = Enum.Font.Michroma,
         TextColor = Color3.fromRGB(255, 255, 255),
         Background = Color3.fromRGB(10, 10, 10),
         Topbar = Color3.fromRGB(20, 20, 20),
-        Accent = Color3.fromRGB(255, 255, 255), -- White Accent
-        BorderColor = Color3.fromRGB(255, 255, 255), -- Pure White Border
+        Accent = Color3.fromRGB(255, 255, 255),
+        BorderColor = Color3.fromRGB(255, 255, 255),
         ElementColor = Color3.fromRGB(25, 25, 25),
-        Rounding = UDim.new(0, 15) -- Max Curvature
+        Rounding = UDim.new(0, 14)
     }
 }
