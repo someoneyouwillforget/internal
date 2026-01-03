af4b771447906bc8aba1163d5e8c945ef9aca27c
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 18:26:11 -0800

    v7

--- source/main.lua
+++ source/main.lua
@@ -1,1 +1,10 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/library.lua"))()
+-- [[ INTERNAL MAIN LOADER ]] --
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/library.lua"))()
+local Theme = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/theme.lua"))().Default
+local Elements = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/elements.lua"))()
+
+-- Link the elements to the library environment
+getgenv().Internal_Elements = Elements
+getgenv().Internal_Theme = Theme
+
+return Library
