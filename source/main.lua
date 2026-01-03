1a417565cb614c3e367a204d40e678b031d3f21b
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 18:37:10 -0800

    i

--- source/main.lua
+++ source/main.lua
@@ -1,10 +1,20 @@
--- [[ INTERNAL MAIN LOADER ]] --
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/library.lua"))()
-local Theme = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/theme.lua"))().Default
-local Elements = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/elements.lua"))()
 
--- Link the elements to the library environment
-getgenv().Internal_Elements = Elements
-getgenv().Internal_Theme = Theme
+local Win = Library:CreateWindow()
+
+-- CREDITS
+local Credits = Win:CreateTab("Credits")
+Credits:CreateHeader({Name = "CREATORS"})
+Credits:CreateButton({Name = "Discord: Copy Link", Callback = function() setclipboard("discord.gg/link") end})
+Credits:CreateButton({Name = "GitHub: @someoneyouwillforget", Callback = function() setclipboard("someoneyouwillforget") end})
+
+-- INFO
+local Info = Win:CreateTab("Info")
+Info:CreateHeader({Name = "PROJECT INFO"})
+Info:CreateButton({Name = "Copy Source Link", Callback = function() setclipboard("github.com/link") end})
+
+-- VISUALS
+local Visuals = Win:CreateTab("Visuals")
+Visuals:CreateHeader({Name = "UI SETTINGS"})
 
 return Library
