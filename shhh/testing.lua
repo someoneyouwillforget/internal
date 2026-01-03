a7538ba7e6f656c0793ed8d7c86b7dda2a0f2420
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 18:23:04 -0800

    v3

--- shhh/testing.lua
+++ shhh/testing.lua
@@ -1,55 +1,37 @@
--- [[ INTERNAL PROJECT ]] --
 local Internal = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/main.lua"))()
 
-local Win = Internal:CreateWindow({
-    Name = "INTERNAL PROJECT"
-})
+local Win = Internal:CreateWindow({ Name = "INTERNAL PROJECT" })
 
--- TAB 1: CREDITS (Flat text, no stroke)
+-- CREDITS
 local Credits = Win:CreateTab("Credits")
 Credits:CreateButton({
-    Name = "Discord Server (Copy Link)",
-    Callback = function() setclipboard("discord.gg/yourserver") end
+    Name = "Discord: Click to Copy",
+    Callback = function() setclipboard("discord.gg/yourlink") end
 })
 Credits:CreateButton({
     Name = "GitHub: @someoneyouwillforget",
     Callback = function() setclipboard("someoneyouwillforget") end
 })
 
--- TAB 2: INFO
+-- INFO
 local Info = Win:CreateTab("Info")
-Info:CreateButton({
-    Name = "GitHub Open Source Link",
-    Callback = function() setclipboard("https://github.com/someoneyouwillforget/internal") end
-})
-Info:CreateButton({
-    Name = "Join Support Discord",
-    Callback = function() setclipboard("discord.gg/yourserver") end
-})
+Info:CreateButton({ Name = "Source Code (GitHub)", Callback = function() setclipboard("https://github.com/someoneyouwillforget/internal") end })
 
--- TAB 3: VISUALS (UI Controls & "Shiy")
+-- VISUALS (With Headers)
 local Visuals = Win:CreateTab("Visuals")
 
+Visuals:CreateHeader({ Name = "UI SETTINGS" }) -- NEW HEADER WITH BORDER
+
 Visuals:CreateToggle({
-    Name = "Menu Rainbow Border",
+    Name = "Rainbow Borders",
     CurrentValue = false,
-    Callback = function(v) print("Rainbow:", v) end
+    Callback = function(v) end
 })
 
+Visuals:CreateHeader({ Name = "MENU CUSTOMIZATION" }) -- NEW HEADER WITH BORDER
+
 Visuals:CreateSlider({
-    Name = "Menu Transparency",
+    Name = "Transparency",
     Min = 0, Max = 100, Default = 0,
-    Callback = function(v) print("Transparency:", v) end
-})
-
-Visuals:CreateToggle({
-    Name = "Show Search Bar",
-    CurrentValue = true,
-    Callback = function(v) print("Search Bar:", v) end
-})
-
-Visuals:CreateSlider({
-    Name = "Tab Border Thickness",
-    Min = 1, Max = 5, Default = 2,
-    Callback = function(v) print("Thickness:", v) end
+    Callback = function(v) end
 })
