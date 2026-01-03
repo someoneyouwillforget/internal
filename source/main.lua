1894c31bee5b83ab7cb604036471c7560eba4e37
someoneyouwillforget <nbgafabtyu@gmail.com>
2026-01-02 18:44:37 -0800

    i

--- source/main.lua
+++ source/main.lua
@@ -1,20 +1,5 @@
+-- [[ INTERNAL MAIN LOADER ]] --
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/someoneyouwillforget/internal/main/source/library.lua"))()
 
-local Win = Library:CreateWindow()
-
--- CREDITS
-local Credits = Win:CreateTab("Credits")
-Credits:CreateHeader({Name = "CREATORS"})
-Credits:CreateButton({Name = "Discord: Copy Link", Callback = function() setclipboard("discord.gg/link") end})
-Credits:CreateButton({Name = "GitHub: @someoneyouwillforget", Callback = function() setclipboard("someoneyouwillforget") end})
-
--- INFO
-local Info = Win:CreateTab("Info")
-Info:CreateHeader({Name = "PROJECT INFO"})
-Info:CreateButton({Name = "Copy Source Link", Callback = function() setclipboard("github.com/link") end})
-
--- VISUALS
-local Visuals = Win:CreateTab("Visuals")
-Visuals:CreateHeader({Name = "UI SETTINGS"})
-
+-- This returns the Library to the test file
 return Library
